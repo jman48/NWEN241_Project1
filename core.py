28fcc7b6b6383665cd1326fbd146c9d55f1a4c75
John Armstrong <j.armstrong484@gmail.com>
2015-04-13 10:07:51 +1200

    Set up most of the core part of the project

---
+++ core.py
@@ -0,0 +1,34 @@
+import urllib.request
+
+def pywget(url):
+    fileName = getFileName(url)
+    print('File ' + fileName + ' downloading....')
+    try:
+        fileInput = urllib.request.urlretrieve(url, fileName)
+        print('File downloaded!')
+    except:
+        print('Network error. Please try again')
+
+
+
+def getFileName(url):
+    lastIndex = 0
+    fileNameDefined = False
+    
+    #find last '/'
+    index = len(url) - 1
+    
+    while index > 0:
+        if (url[index] == '/'):
+            lastIndex = index + 1 #Plus 1 so we exclude the '/'
+            break
+        if (url[index] == '.'): #If we do not find a '.' before the '/' the the file name must not be defined
+            fileNameDefined = True
+        index = index - 1
+        
+    if (fileNameDefined):
+        return url[lastIndex:]
+    return 'index.html'
+
+def checkExists(fileName):
+    
