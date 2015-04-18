39038351dd676f0e5521e5125b0761115c6d1b7b
John Armstrong <j.armstrong484@gmail.com>
2015-04-18 14:37:46 +1200

    Finished the complection part of the project

--- completion.py
+++ completion.py
@@ -3,65 +3,113 @@ from urllib.parse import urlparse
 import urllib.request
 import os
 
+
 def pywget(url):
-    #Get root file
+    # Get root file
     rootFileName = getFileName(url)
     downLoadFile(url, rootFileName)
     rootUrl = urlparse(url)
-    
-    htmlParser = getLinkedFiles(rootFileName)
+
+    #Do not try and download links if file is not an html file
+    if getExtension(rootFileName) == 'html' or getExtension(rootFileName) == 'htm':
+        downloadlinks(rootFileName, url)
+
+
+def downloadlinks(rootfilename, url):
+    """
+    Download all the links in the provided file if they are from the same domain as the url provided
+
+    :param rootfilename: The name of the root html file to download the links from
+    :param url: The url of the root html file. Used to check if links are from the same domain
+    :return:
+    """
+    rooturl = urlparse(url)
+    htmlParser = getLinkedFiles(rootfilename)
     for index, link in enumerate(htmlParser.links):
-        #print(htmlParser.positions[index])
+        # print(htmlParser.positions[index])
         linkUrl = urlparse(link[1])
-        linkFileName = getFileName(link[1])        
+        linkFileName = getFileName(link[1])
 
         if (linkUrl.netloc == ''):
             #If the link is relative then get the path to the link from the root url
             downLoadFile(getPath(url, getUrlFileName(url)) + linkUrl.path, linkFileName)
-            updateRootLink(rootFileName, link[0], linkFileName, htmlParser.positions[index])
-        elif (linkUrl.netloc == rootUrl.netloc):
+            updateRootLink(rootfilename, link[0], linkFileName, htmlParser.positions[index])
+        elif (linkUrl.netloc == rooturl.netloc):
             downLoadFile(linkUrl.geturl(), linkFileName)
-            updateRootLink(rootFileName, link[0], linkFileName, htmlParser.positions[index])
-    """ Update reference in root file """
+            updateRootLink(rootfilename, link[0], linkFileName, htmlParser.positions[index])
+
 
 def updateRootLink(rootFileName, linkType, linkFileName, pos):
+    """
+    Update the link at pos to use the linkfilename.
+
+    :param rootFileName: The name of the root html file containing the link
+    :param linkType: The type of link. Either 'src' or 'href'
+    :param linkFileName: The name of the local file to update link to
+    :param pos: The position of the html tag that contains the link to update. pos[0] = line of link. pos[1] = line offset of link
+    :return:
+    """
     rootFile = open(rootFileName, 'r')
     rootData = rootFile.readlines()
     rootFile.close()
 
     line = pos[0] - 1
-    tag = rootData[line][pos[1]:] #pos[0] contains line number. pos[1] contains line offset
-    linkStart = tag.find(linkType + '="')  #Find either src=" or href=" in the tag
+    tag = rootData[line][pos[1]:]  # pos[0] contains line number. pos[1] contains line offset
+    linkStart = tag.find(linkType + '="')  # Find either src=" or href=" in the tag
     indexStart = tag.find('"', linkStart)
     indexEnd = tag.find('"', indexStart + 1) + 1
-    rootData[line] = rootData[line].replace(tag, overWriteLink(indexStart, indexEnd, tag, linkFileName), 1)
-    print(rootData[line])
-    
+    rootData[line] = rootData[line].replace(tag, insert(indexStart, indexEnd, tag, linkFileName), 1)
+
     rootFile = open(rootFileName, 'w')
     rootFile.writelines(rootData)
     rootFile.close()
 
-def overWriteLink(start, end, text, new):
+
+def insert(start, end, text, new):
+    """
+    Insert the string 'new' into the text based on the start and end indexes
+
+    :param start: Where to start the text insertion
+    :param end: Where to end the text insertion
+    :param text: The text to insert the new text into
+    :param new: The text to insert
+    :return:
+    """
     return text[:start] + new + text[end:]
-    
+
+
 def getPath(url, fileName):
-    """ Returns the full url minus the file name. Makes it easy to get other files in the same directory"""
+    """
+    Returns the full url minus the file name. Makes it easy to get other files in the same directory
+
+    :param url: The url of the file
+    :param fileName: The url filename. This should not be the collision renamed filename. i.e not cat.0.jpg
+    :return:
+    """
     index = url.find(fileName)
     return url[:index]
 
+
 def getLinkedFiles(fileName):
-    """ Get all files that are linked from the file specified """
+    """
+    Get all files that are linked from the file specified
+
+    :param fileName: The file name to get all the <a> and <img> links from
+    :return: An htmlparser with the links stored in 'links'
+    """
     file = open(fileName, 'r')
     h = HTMLlinks()
     h.feed(file.read())
     file.close()
     return h
-        
+
 
 def downLoadFile(url, fileName):
     """
-    Download a file to disk. Will nmake sure that the downloaded files name
-    is unique. returns the file input stream
+    Download a file to disk. Will make sure that the downloaded file name is unique.
+    :param url: The url of the file to download
+    :param fileName: What to name the downloaded file
+    :return:
     """
     print('File ' + fileName + ' downloading....')
     try:
@@ -70,51 +118,45 @@ def downLoadFile(url, fileName):
     except:
         print('Network error downloading from "' + url + '". Please try again')
 
+
 def getFileName(url):
     """
     This function gets the file name for the url that will be saved to disk.
-    
+
     This may involve adding a number to the file name if there exists a file with
     the same name
+
+    :param url: The url of the file
+    :return: The file name
     """
     fileName = getUrlFileName(url)
-    if (os.path.exists(fileName)):
+    if os.path.exists(fileName):
         fileName = addPrefixNum(fileName, 0)
     return fileName
+
 
 def getUrlFileName(url):
     """
     This function gets the filename from the url provided.
 
     If no file name is specified then it returns index.html as the file name
-    """
-    lastIndex = 0
-    fileNameDefined = False
-    
-    #find last '/'
-    index = len(url) - 1
-    
-    while index > 0:
-        if (url[index] == '/'):
-            lastIndex = index + 1 #Plus 1 so we exclude the '/'
-            break
-        #If we do not find a '.' before the '/' then the file name must not be defined
-        if (url[index] == '.'): 
-            fileNameDefined = True
-        index = index - 1
 
-    #Return the filename or index.html if the file name is not defined
-    if (fileNameDefined):
-        return url[lastIndex:]
+    :param url:
+    """
+    sections = url.split('/')
+    if sections[len(sections) - 1].find('.') > -1:
+        return sections[len(sections) - 1]
     return 'index.html'
+
 
 def addPrefixNum(fileName, times):
     """
-    This recursive function will add a number to the file name so that is is unique. i.e index.1.html
+    Recursively add a number before the files extension until it is unique
     """
-    if (os.path.exists(getPrefixedName(fileName, times))):
-        return addPrefixNum(fileName, times+1)
+    if os.path.exists(getPrefixedName(fileName, times)):
+        return addPrefixNum(fileName, times + 1)
     return getPrefixedName(fileName, times);
+
 
 def getPrefixedName(fileName, prefix):
     """
@@ -122,27 +164,36 @@ def getPrefixedName(fileName, prefix):
     """
 
     index = len(fileName) - 1;
-    while (index > 0):
-        if (fileName[index] == '.'):
+    while index > 0:
+        if fileName[index] == '.':
             return fileName[:index] + '.' + str(prefix) + fileName[index:]
-        index = index-1
-    #Do something as we did not find the file extension
+        index += 1
+
+
+def getExtension(fileName):
+
+    sections = fileName.split('.')
+    return sections[len(sections) - 1]
+
 
 class HTMLlinks(HTMLParser):
-    """ A custom html parser class to get and store all links in an html ducument """
+    """
+    A custom html parser class to get and store all links in an html document
+    """
+
     def __init__(self):
         HTMLParser.__init__(self)
         self.links = []
         self.positions = []
-        
+
     def handle_starttag(self, tag, attrs):
         """ Get all links from an html document """
-        
-        #Get all href values from an 'a' tag
-        if(tag == 'a' and attrs[0][0] =='href'):
+
+        # Get all href values from an 'a' tag
+        if tag == 'a' and attrs[0][0] == 'href':
             self.positions.append(self.getpos())
             self.links.append(attrs[0])
-        #Get all src values from all 'img' tags
-        elif(tag == 'img' and attrs[0][0] =='src'):
+        # Get all src values from all 'img' tags
+        elif tag == 'img' and attrs[0][0] == 'src':
             self.positions.append(self.getpos())
             self.links.append(attrs[0])
